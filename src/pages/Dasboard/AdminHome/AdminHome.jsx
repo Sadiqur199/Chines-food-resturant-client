287b01c856eca6b1b0e50db75aa69583b619bf1a
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-09-11 02:20:10 +0600

    added admin status data

--- src/pages/Dasboard/AdminHome/AdminHome.jsx
+++ src/pages/Dasboard/AdminHome/AdminHome.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import UseAuth from '../../../Hooks/UseAuth';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const AdminHome = () => {
   const {user} = UseAuth()
+  const [axiosSecure] = useAxiosSecure()
+
+  const {data: stats = {}} = useQuery({
+    queryKey : ['/admin-stats'],
+    queryFn: async () =>{
+      const res = await axiosSecure('/admin-stats')
+      return res.data;
+    }
+  })
+
   return (
     <div className='w-full m-4'>
-      <h2 className='text-3xl'>Welcome Back , {user.displayName}</h2>
+      <h2 className='text-3xl'>Hi! ,Welcome  {user.displayName}</h2>
     </div>
   );
 };
