3133eb0b335ce6496dd277e2f40351230d60f5d1
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-07-02 03:18:15 +0600

    update comlete and newly push this project

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,19 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey:import.meta.env.VITE_apiKey,    
-  // authDomain:import.meta.env.VITE_authDomain,
-  // projectId:import.meta.env.VITE_projectId,
-  // storageBucket:import.meta.env.VITE_storageBucket,
-  // messagingSenderId:import.meta.env.VITE_messagingSenderId, 
-  // appId:import.meta.env.VITE_appId 
+  apiKey:import.meta.env.VITE_apiKey,    
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId, 
+  appId:import.meta.env.VITE_appId 
 
-  apiKey: "AIzaSyCiWVa-KOCwsr0C3K-qfGNUw8GIW4KSyVM",
-  authDomain: "chines-food-restaurant.firebaseapp.com",
-  projectId: "chines-food-restaurant",
-  storageBucket: "chines-food-restaurant.appspot.com",
-  messagingSenderId: "977245395723",
-  appId: "1:977245395723:web:90d8640f3f491185d4200e"
+  
 };
 
 // Initialize Firebase
